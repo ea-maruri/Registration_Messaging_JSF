f53a2c46620e7a3602420970e421ca88a9cf5025
ea-maruri <64037074+ea-maruri@users.noreply.github.com>
2020-10-18 20:14:54 -0500

    update classes to allow messages removing (between a HashMap) using its id

---
+++ src/main/java/com/corejsf/msg/MessageToRemoveBean.java
@@ -0,0 +1,22 @@
+package com.corejsf.msg;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+/**
+ * @author EAMT on 18/10/2020
+ */
+@Named("messageToRemove")
+@RequestScoped
+public class MessageToRemoveBean {
+    private int id = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
